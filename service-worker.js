af957315ce4f28e41e1a013849bad7f24c338121
Sekiruto <73583237+Sekiruto@users.noreply.github.com>
2025-09-22 14:20:09 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,3 @@
-// Simple SW for offline shell
-const CACHE = 'questboard-v1';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./index.html','./manifest.json'])));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => self.clients.claim());
-self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url);
-  if (url.origin === location.origin) {
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-  }
-});
+// placeholder service worker
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('fetch',()=>{});
